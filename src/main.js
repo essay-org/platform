85e71bb61f6c34f6fcead1677c300ae9be8f8da2
wmui <qq22337383@gmail.com>
2022-07-19 11:46:35 +0800

    feat: add layout

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
-import './style.css'
+import Antd from 'ant-design-vue'
 import App from './App.vue'
+import 'ant-design-vue/dist/antd.css'
 
-createApp(App).mount('#app')
+createApp(App).use(Antd).mount('#app')
---
+++ src/views/layout/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <a-layout class="layout">
+    <a-layout-sider
+      class="sider"
+      v-model:collapsed="collapsed"
+      :width="asiderWidth"
+      :trigger="null"
+      collapsible
+    >
+    </a-layout-sider>
+    <a-layout-header></a-layout-header>
+    <a-layout-content></a-layout-content>
+    <a-layout-footer></a-layout-footer>
+  </a-layout>
+</template>
+
+<script>
+import { computed, defineComponent, ref } from "vue";
+
+export default defineComponent({
+  setup() {
+    const collapsed = ref(false);
+    const asiderWidth = computed(() => (collapsed.value ? 80 : 220));
+
+    return {
+      asiderWidth,
+      collapsed,
+    };
+  },
+});
+</script>
